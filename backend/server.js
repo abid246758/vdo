253fd6d43edd3e15afe3ef924b3ac16b89a65cf2
Abid Hassan <137408314+abid246758@users.noreply.github.com>
2025-10-01 14:34:03 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const http = require('http');
 const cors = require('cors');
+const path = require('path');
 const helmet = require('helmet');
 const rateLimit = require('express-rate-limit');
 const { Server } = require('socket.io');
